b34ac282b88456d1ee5ad8d213d9e1d36be4adc0
Vasily Khayrulin <sirianru@gmail.com>
2013-03-20 15:27:55 +0400

    Добавлено отображение времени запросов в developer toolbar

--- src/Sirian/SphinxBundle/Sphinx/SphinxClient.php
+++ src/Sirian/SphinxBundle/Sphinx/SphinxClient.php
@@ -25,10 +25,41 @@ class SphinxClient extends \SphinxClient implements ContainerAwareInterface
         return $this->container;
     }
 
+    public function execQueries()
+    {
+        $results = parent::RunQueries();
+        $this->_reqs = array ();
+
+        if (!is_array($results)) {
+            throw new SphinxException();
+        }
+
+
+        $this->_error = $results[0]["error"];
+        $this->_warning = $results[0]["warning"];
+
+        if ($results[0]["status"] == SEARCHD_ERROR) {
+            throw new SphinxException($this->GetLastError());
+        } else {
+            return $results[0];
+        }
+    }
+
     public function query($query, $index = '*', $comment = '')
     {
+        if ($this->container->has('debug.stopwatch')) {
+            $stopwatch = $this->container->get('debug.stopwatch');
+            $stopwatch->start('sphinx');
+        }
+
         $res = parent::query($query, $index, $comment);
-        if (false === $res) {
+
+        if ($this->container->has('debug.stopwatch')) {
+            $stopwatch = $this->container->get('debug.stopwatch');
+            $stopwatch->stop('sphinx');
+        }
+
+        if (!is_array($res)) {
             throw new SphinxException($this->GetLastError());
         }
         return $res;
